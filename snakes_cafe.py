b7efc92de475615e3d5eefc68ea64c40a9af4a20
Tanesha Brester <taneshabrester@gmail.com>
2022-08-02 16:41:51 -0700

    edit

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -1,3 +1,4 @@
+# def = define function, then calling the welcome function
 def welcome():
     print("""
     **************************************
@@ -38,6 +39,36 @@ def menu():
     """)
 
 
+def menu_counter():
+    list = {
+        "Wings": 0,
+        "Cookies": 0,
+        "Spring Rolls": 0,
+        "Salmon": 0,
+        "Steak": 0,
+        "Meat Tornado": 0,
+        "A Literal Garden": 0,
+        "Ice Cream": 0,
+        "Cake": 0,
+        "Pie": 0,
+        "Coffee": 0,
+        "Tea": 0,
+        "Unicorn Tears": 0
+    }
+
+
+order = input(">")
+if order in list:
+    list[order] += 1
+elif order == "quit":
+    exit()
+elif order == "submit":
+    for item in list:
+        if list[item] > 0:
+            print(f'{item}:{list[item]}')
+else:
+    print("item is not on the menu")
+
 def orders():
     print("""***********************************
     ** What would you like to order? **
@@ -45,11 +76,19 @@ def orders():
     """)
 
 
+# only execute code below when this file is directly executed from command line
 if __name__ == '__main__':
+    # what do I want to have show up on screen first? welcome
+    welcome()
 
-    order = input(">")
+    # what I want to show up on the screen next is menu
+    menu()
 
-# create data structure that menu items and quantity
-    num_item = 1
-    report = f" {num_item} order of {order} have been added to your meal "
-    print(report)
+    # what I want to show up next
+    orders()
+
+#
+# # create data structure that menu items and quantity
+#     num_item = 1
+#     report = f" {num_item} order of {order} have been added to your meal "
+#     print(report)
