e72a5be40797463ba5984ff5ba22e75b07bd129e
Tanesha Brester <taneshabrester@gmail.com>
2022-08-09 17:35:34 -0700

    finished the ordering part completed lab

---
+++ README.md
@@ -0,0 +1,20 @@
+# Snakes Cafe
+
+**Author**: Tanesha Brester
+[PR URL]())
+
+## Overview
+User can submit their order from the default menu. Upon submission, the user is able to see their order and how many of the food item they have ordered.
+
+## Architecture
+Python
+
+### Lab 1 - Frontend
+Name of feature: snakes-cafe
+Estimate of time needed to complete: 4 hours
+Start time: Friday 3:00pm
+Finish time: Tuesday 5:40pm
+Actual time needed to complete: a couple days, i had to come back to this lab
+
+## Credit and Collaborations
+Brandon M
--- snakes_cafe.py
+++ snakes_cafe.py
@@ -1,94 +1,77 @@
-# def = define function, then calling the welcome function
-def welcome():
+def welcomeMessage():
     print("""
-    **************************************
-    **    Welcome to the Snakes Cafe!   **
-    **    Please see our menu below.    **
-    **
-
-    ** To quit at any time, type "quit" **
-    **************************************
-    """)
-
-
-def menu():
-    print(""" Appetizers
-    ----------
-    Wings
-    Cookies
-    Spring Rolls
-
-    Entrees
-    -------
-    Salmon
-    Steak
-    Meat Tornado
-    A Literal Garden
-
-    Desserts
-    --------
-    Ice Cream
-    Cake
-    Pie
-
-    Drinks
-    ------
-    Coffee
-    Tea
-    Unicorn Tears
-    """)
-
-
-def menu_counter():
+**************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**
+** To quit at any time, type "quit" **
+**************************************
+        """)
+def displayMenu():
+    print("""
+Appetizers
+----------
+Wings
+Cookies
+Spring Rolls
+Entrees
+-------
+Salmon
+Steak
+Meat Tornado
+A Literal Garden
+Desserts
+--------
+Ice Cream
+Cake
+Pie
+Drinks
+------
+CoffeeWing
+Tea
+Unicorn Tears
+          """)
+def submitPrompt():
+    print("""
+************************************
+** What would you like to order?  **
+** To submit order, type "Submit" **
+************************************
+        """)
+def menuCounter():
     list = {
-        "Wings": 0,
-        "Cookies": 0,
-        "Spring Rolls": 0,
-        "Salmon": 0,
-        "Steak": 0,
-        "Meat Tornado": 0,
-        "A Literal Garden": 0,
-        "Ice Cream": 0,
-        "Cake": 0,
-        "Pie": 0,
-        "Coffee": 0,
-        "Tea": 0,
-        "Unicorn Tears": 0
-    }
-
-
-order = input(">")
-if order in list:
-    list[order] += 1
-elif order == "quit":
-    exit()
-elif order == "submit":
-    for item in list:
-        if list[item] > 0:
-            print(f'{item}:{list[item]}')
-else:
-    print("item is not on the menu")
-
-def orders():
-    print("""***********************************
-    ** What would you like to order? **
-    ***********************************
-    """)
-
-
-# only execute code below when this file is directly executed from command line
-if __name__ == '__main__':
-    # what do I want to have show up on screen first? welcome
-    welcome()
-
-    # what I want to show up on the screen next is menu
-    menu()
-
-    # what I want to show up next
-    orders()
-
-#
-# # create data structure that menu items and quantity
-#     num_item = 1
-#     report = f" {num_item} order of {order} have been added to your meal "
-#     print(report)
+"Wings": 0,
+"Cookies": 0,
+"Spring Rolls": 0,
+"Salmon": 0,
+"Steak": 0,
+"Meat Tornado": 0,
+"A Literal Garden": 0,
+"Ice Cream": 0,
+"Cake": 0,
+"Pie": 0,
+"CoffeeWing": 0,
+"Tea": 0,
+"Unicorn Tears": 0
+}
+    status = True
+    displayTheOrder = "** {} order of {} have been added to your meal **"
+    while status == True:
+        orderedItem = input("> ")
+        if orderedItem in list:
+            list[orderedItem] += 1
+            print(displayTheOrder.format(list[orderedItem], orderedItem))
+        elif orderedItem == "quit":
+            exit()
+        elif orderedItem == "Submit":
+            print("**           Your order:           **")
+            for item in list:
+                if list[item] > 0:
+                    print(f'Total {item}: {list[item]}')
+        else:
+            print("Item not on Menu")
+if __name__=="__main__":
+  welcomeMessage()
+  displayMenu()
+  submitPrompt()
+  menuCounter()
